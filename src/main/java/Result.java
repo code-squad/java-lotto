a2688b0f58d6c166afa0eab307635b48b22319bd
= <rightgear11@gmail.com>
2017-12-05 00:44:41 +0900

    Modify merethings

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,5 +1,4 @@
 
-
 public class Result {
 	
 	static void showMyLottos(Lottos lottos) {
--- src/test/java/UserLottoTest.java
+++ src/test/java/UserLottoTest.java
@@ -7,7 +7,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LottoTest {
+public class UserLottoTest {
 	
 	UserLotto userLotto;
 	
