066a5f5fcba8a3c25db016ba2b2982fa7393d29d
trytocloud <eunji.kang@gmail.com>
2018-01-14 00:57:42 +0900

    remove unnecessary new line

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -7,7 +7,6 @@ public class LottoGenerator {
     private static final int NUMBER_COUNT = 6;
 
     private static Random random = new Random();
-
     private static List<Integer> NUMBERS;
     static {
         NUMBERS = new ArrayList<>();
