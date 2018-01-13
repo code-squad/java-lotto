36786ad4cb133cf3d0c6da4e0871a2b4049010af
rogue <retrokarr@gmail.com>
2018-01-13 21:06:15 +0900

    refactor : LottoGame test complete

--- src/test/java/domain/LottoGameTest.java
+++ src/test/java/domain/LottoGameTest.java
@@ -2,13 +2,21 @@ package domain;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoGameTest {
-
-    //당첨 리스트를 받아서 결과값의 리스트를 반환한다.
-/*    @Test
+    @Test
     public void getLottoResultState() {
-        LottoGame lottoGame = new LottoGame();
-    }*/
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        LottoGame lottoGame = new LottoGame(lottos);
+        assertThat(lottoGame.getLottos()).isEqualTo(lottos);
+    }
 }
