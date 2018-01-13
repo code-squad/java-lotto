0e4764fbad50a96c39961e5645570532502933a0
rogue <retrokarr@gmail.com>
2018-01-13 17:06:58 +0900

    refactor : devide method

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,19 +6,28 @@ import java.util.List;
 
 public class Lotto {
     public static final int LOTTO_POOL_NUM = 6;
-    List<Integer> nums;
+    public static final int LOTTO_POOL_MAX_NUM = 45;
+    private List<Integer> nums;
 
     Lotto() {
-        nums = createNums();
+        nums = getSubNums(shuffleNums(createNums()));
     }
 
     private List<Integer> createNums() {
-        List<Integer> nums = new ArrayList<>(45);
-        for (int i = 1; i <= 45; i++)
+        List<Integer> nums = new ArrayList<>(LOTTO_POOL_MAX_NUM);
+        for (int i = 1; i <= LOTTO_POOL_MAX_NUM; i++)
             nums.add(i);
 
+        return nums;
+    }
+
+    private List<Integer> shuffleNums(List<Integer> nums) {
         Collections.shuffle(nums);
 
+        return nums;
+    }
+
+    private List<Integer> getSubNums(List<Integer> nums) {
         return nums.subList(0, LOTTO_POOL_NUM);
     }
 
--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -2,10 +2,13 @@ package domain;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class LottoTest {
     @Test
     public void createLottoTicketTest() {
         Lotto lotto = new Lotto();
+        
         System.out.println(lotto);
     }
 }
