12ab67561649bdae8c1d22901cf5d3ad8ec93f24
trytocloud <eunji.kang@gmail.com>
2018-01-14 00:33:46 +0900

    Remove input guide message

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class InputView {
     public int requestToInputTicketTotalPrice() {
-        System.out.println("로또 1장의 가격은 1000원이다.");
         System.out.println("구입금액을 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
