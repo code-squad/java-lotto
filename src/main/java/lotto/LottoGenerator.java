fb261efb4ad939f21178c3f00e8117bdc87fe220
trytocloud <eunji.kang@gmail.com>
2018-01-14 13:27:43 +0900

    Improve generating random numbers

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -5,23 +5,13 @@ import lotto.domain.LottoConstants;
 import java.util.*;
 
 public class LottoGenerator {
-    private static Random random = new Random();
-    private static List<Integer> NUMBERS;
-    static {
-        NUMBERS = new ArrayList<>();
+    public static List<Integer> randomNumbers() {
+        List<Integer> NUMBERS = new ArrayList<>();
         for (int i = LottoConstants.FIRST_NUMBER; i <= LottoConstants.LAST_NUMBER; i++) {
             NUMBERS.add(i);
         }
-    }
-
-    public static List<Integer> randomNumbers() {
         Collections.shuffle(NUMBERS);
 
-        Set<Integer> randomNumbers = new HashSet<>();
-        while (randomNumbers.size() != LottoConstants.NUMBER_COUNT) {
-            int randomNumber = NUMBERS.get(random.nextInt(LottoConstants.LAST_NUMBER));
-            randomNumbers.add(randomNumber);
-        }
-        return new ArrayList<>(randomNumbers);
+        return NUMBERS.subList(LottoConstants.FIRST_NUMBER, LottoConstants.FIRST_NUMBER + LottoConstants.NUMBER_COUNT);
     }
 }
