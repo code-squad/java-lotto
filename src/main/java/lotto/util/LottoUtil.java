7b040ad8544dd81e3ca74582b4a5ad285f5b94a6
derren <derren832@gmail.com>
2018-01-15 12:49:07 +0900

    로또 구매 및 당첨 번호 입력 기능 구현

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,5 +1,17 @@
 package lotto.util;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+
+import java.util.List;
+
 public class LottoUtil {
 
+    public static Lotto parseLotto(List<Integer> numbers) {
+        LottoNumber[] numberArr = new LottoNumber[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            numberArr[i] = new LottoNumber(numbers.get(i));
+        }
+        return new Lotto(numberArr);
+    }
 }
--- src/test/java/lotto/domain/LottoVendorTest.java
+++ src/test/java/lotto/domain/LottoVendorTest.java
@@ -7,11 +7,36 @@ import static org.junit.Assert.assertEquals;
 public class LottoVendorTest {
     @Test
     public void 입력을_파싱한다() {
-        assertEquals(16000 ,LottoVendor.parseMoney("16000"));
+        assertEquals(16000 ,LottoVendor.getInstance().parseMoney("16000"));
     }
-/**
- * 입력값을 받는다.
- * 1000단위로 나눈다.
- * 나눈값으로 로또를 구매한다.
- */
+
+    @Test
+    public void 파싱값을_로또의_단위로_연산_한다() {
+        assertEquals(16 ,LottoVendor.getInstance().unitPerLotto(16000));
+    }
+
+    @Test
+    public void 로또를_구매한다() {
+        assertEquals(16 ,LottoVendor.getInstance().buy("16000").size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 지난주_로또_당첨_번호를_null() {
+        LottoVendor.getInstance().setLastJackpotLotto("   \n");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 지난주_로또_당첨_번호를_입력_기호_예외처리() {
+        LottoVendor.getInstance().setLastJackpotLotto("r1,2,3,4,5,6d");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 지난주_로또_당첨_번호를_입력_개수_부족() {
+        LottoVendor.getInstance().setLastJackpotLotto("1,2");
+    }
+
+    @Test
+    public void 지난주_로또_당첨_번호를_입력() {
+        LottoVendor.getInstance().setLastJackpotLotto("1,2,3,4,5,6");
+    }
 }
