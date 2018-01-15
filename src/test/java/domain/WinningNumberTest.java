2c9de4fd139d982bbdd85654fa1c7f1ec362847c
rogue <retrokarr@gmail.com>
2018-01-15 14:28:55 +0900

    feat : input validation check

--- src/test/java/domain/WinningNumberTest.java
+++ src/test/java/domain/WinningNumberTest.java
@@ -13,4 +13,19 @@ public class WinningNumberTest {
         assertThat(winningNumber.getWinningLottos()).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6));
         assertThat(winningNumber.getBonusNum()).isEqualTo(7);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidInputTest_null_case() {
+        new WinningNumber(null, 7);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidInputTest_not_enough_case() {
+        new WinningNumber(Arrays.asList(1, 2, 3, 4, 5), 7);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidInputTest_negative_case() {
+        new WinningNumber(Arrays.asList(1, 2, 3, 4, 5, 6), -1);
+    }
 }
