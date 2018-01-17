0d8c318877a9187f97cd95a6137feab5018028f7
young891221 <young891221@gmail.com>
2018-01-17 17:28:37 +0900

    피드백 : 테스트 Lottos 생성도 Input의 customLottos 메서드를 사용하여 간결하게 작성

--- src/test/java/LottoMachineTest.java
+++ src/test/java/LottoMachineTest.java
@@ -27,6 +27,8 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class LottoMachineTest {
+    private static String stringWinNumbers = "1, 2, 3, 4, 5, 6";
+    private static int bonusBall = 7;
 
     @Test
     public void 금액에_맞는_개수인가() {
@@ -43,7 +45,7 @@ public class LottoMachineTest {
 
     @Test
     public void 당첨번호_통계결과가_일치하는가() {
-        Map<Rank, Integer> resultMap = initResultMap("1, 2, 3, 4, 5, 6", 7);
+        Map<Rank, Integer> resultMap = initResultMap(stringWinNumbers, bonusBall);
 
         assertEquals(of(2).get(), ofNullable(resultMap.get(THREE)).orElse(0));
         assertEquals(of(0).get(), ofNullable(resultMap.get(FOUR)).orElse(0));
@@ -54,26 +56,23 @@ public class LottoMachineTest {
 
     @Test
     public void 당첨되지_않으면_비어있는_값을_반환하는가() {
-        assertTrue(initResultMap("21, 22, 23, 24, 25, 26", 17).isEmpty());
+        String notWinNumbers = "21, 22, 23, 24, 25, 26";
+        int notMoneyBall = 17;
+        assertTrue(initResultMap(notWinNumbers, notMoneyBall).isEmpty());
     }
 
     @Test
     public void 총수익이_올바른가() {
-        assertEquals(2031510000, LottoResult.getProfit(initResultMap("1, 2, 3, 4, 5, 6", 7)));
+        assertEquals(2031510000, LottoResult.getProfit(initResultMap(stringWinNumbers, bonusBall)));
     }
 
     @Test
     public void 총수익률이_올바른가() {
-        assertThat(LottoResult.getProfitRate(LottoResult.getProfit(initResultMap("1, 2, 3, 4, 5, 6", 7)), 6000), is(3.38585E7F));
+        assertThat(LottoResult.getProfitRate(LottoResult.getProfit(initResultMap(stringWinNumbers, bonusBall)), 6000), is(3.38585E7F));
     }
 
     private Map<Rank, Integer> initResultMap(String stringWinNumbers, int bonusBall) {
-        List<Lotto> testLottos = new ArrayList<>();
-        testLottos.add(Lotto.generate(() -> Arrays.asList(1, 2, 3, 40, 41, 42))); //THREE
-        testLottos.add(Lotto.generate(() -> Arrays.asList(1, 2, 3, 14, 15, 16))); //THREE
-        testLottos.add(Lotto.generate(() -> Arrays.asList(1, 2, 3, 4, 5, 16))); //FIVE
-        testLottos.add(Lotto.generate(() -> Arrays.asList(11, 2, 7, 4, 5, 6))); //FIVE_DOUBLE
-        testLottos.add(Lotto.generate(() -> Arrays.asList(1, 2, 3, 4, 5, 6))); //SIX
+        List<Lotto> testLottos =Input.init("1, 2, 3, 40, 41, 42\n1, 2, 3, 14, 15, 16\n1, 2, 3, 4, 5, 16\n11, 2, 7, 4, 5, 6\n1, 2, 3, 4, 5, 6").customLottoms(5);
         Lottos lottos =  Lottos.generate(testLottos);
 
         Lotto lotto = (Input.init(stringWinNumbers).winNumbers());
