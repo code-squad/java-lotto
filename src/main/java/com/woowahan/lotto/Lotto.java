0d09f2516efe2f293e41693564a3ed12688278ea
HannaJeon <jks7psj@gmail.com>
2018-01-17 17:48:08 +0900

    Lotto class 생성

---
+++ src/main/java/com/woowahan/lotto/Lotto.java
@@ -0,0 +1,29 @@
+package com.woowahan.lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private List<Integer> baseNumberList;
+
+    public Lotto() {
+        baseNumberList = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            baseNumberList.add(i);
+        }
+    }
+    public List<Integer> create() {
+        List<Integer> copyBaseList = new ArrayList<>(baseNumberList);
+        Collections.shuffle(copyBaseList);
+        return copyBaseList.subList(0, 6);
+    }
+
+    public List<List<Integer>> buyLotto(int money) {
+        List<List<Integer>> totalLotto = new ArrayList<>();
+        for (int i = 0; i < money/1000; i++) {
+            totalLotto.add(create());
+        }
+        return totalLotto;
+    }
+}
