3a484262f0e1fff3a631534a17d7f5c14b1d458d
ttaeyoung <ttaeyoung@gmail.com>
2018-01-23 13:26:12 +0900

    test(step3): add test

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -9,27 +9,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WinningLottoTest {
     @Test
     public void match() {
-        WinningLotto wLotto = new WinningLotto(Arrays.asList(1,2,3,4,5,6), 7);
+        WinningLotto wLotto = new WinningLotto("1,2,3,4,5,6", 7);
 
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Lotto lotto = new Lotto("1,2,3,4,5,6");
         assertThat(wLotto.match(lotto)).isEqualTo(6);
 
-        Lotto matchFive = new Lotto(Arrays.asList(1,2,3,4,5,9));
+        Lotto matchFive = new Lotto("1,2,3,4,5,9");
         assertThat(wLotto.match(matchFive)).isEqualTo(5);
 
-        Lotto matchNone = new Lotto(Arrays.asList(10,11,23,14,15,16));
+        Lotto matchNone = new Lotto("10,11,23,14,15,16");
         assertThat(wLotto.match(matchNone)).isEqualTo(0);
 
     }
 
     @Test
     public void isBonus() {
-        WinningLotto wLotto = new WinningLotto(Arrays.asList(1,2,3,4,5,6), 7);
+        WinningLotto wLotto = new WinningLotto("1,2,3,4,5,6", 7);
 
-        Lotto match = new Lotto(Arrays.asList(1,2,3,4,5,7));
+        Lotto match = new Lotto("1,2,3,4,5,7");
         assertThat(wLotto.isBonus(match)).isTrue();
 
-        Lotto notMatch = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Lotto notMatch = new Lotto("1,2,3,4,5,6");
         assertThat(wLotto.isBonus(notMatch)).isFalse();
 
     }
