f39bd9fa529e27d2ce860665f24891f2ad2f8eca
jimmy <foodsksms@gmail.com>
2018-03-15 18:53:55 +0900

    numbers초기화

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,23 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private List<Integer> lotto;
     static ArrayList<Integer> counts = new ArrayList<>();
-    ArrayList<Integer> numbers = makeNumbers();
+    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45);
 
     public Lotto() {
         Collections.shuffle(numbers);
         lotto = numbers.subList(0, 6);
-    }
-
-    public static ArrayList<Integer> makeNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for(int i = 1; i < 46; i++)
-            numbers.add(i);
-        System.out.println("로또 새로생성");
-        return numbers;
     }
 
     public List<Integer> getLotto() {
