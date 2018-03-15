36947bb96ba73b97894a25d5be647d4002a53dc7
jimmy <foodsksms@gmail.com>
2018-03-15 15:18:22 +0900

    피드백 수정

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -23,39 +23,13 @@ public class Money {
     }
 
     public ArrayList<Integer> countAdd(ArrayList<Integer> counts, int i) {
-        int num = Integer.parseInt(String.valueOf(counts.get(i)));
-//        int num = finalCount.get(String.valueOf(i));
+        int num = counts.get(i);
+        String key = String.valueOf(num);
         if (num > 2) {
-            finalCount.put(String.valueOf(i), ++num);
+            int value = finalCount.get(key);
             totalMoney.add(addMoney.get(num - 3));
+            finalCount.put(key, ++value);
         }
-//        System.out.println("value of count.get is " + String.valueOf(counts.get(i)));
-
-//        if(counts.get(i) == 3) {
-//            int num = finalCount.get("3");
-//            finalCount.put("3", ++num);
-//            totalMoney.add(5000);
-//        }
-//
-//
-//        if(counts.get(i) == 4) {
-//            int num = finalCount.get("4");
-//            finalCount.put("4", ++num);
-//            totalMoney.add(50000);
-//        }
-//
-//        if(counts.get(i) == 5) {
-//            int num = finalCount.get("5");
-//            finalCount.put("5", ++num);
-//            totalMoney.add(1500000);
-//        }
-//
-//        if(counts.get(i) == 6) {
-//            int num = finalCount.get("6");
-//            finalCount.put("6", ++num);
-//            totalMoney.add(2000000000);
-//        }
-
         return totalMoney;
     }
 
