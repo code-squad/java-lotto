09a4dfbdbc3ccd2ca15996f91dfa4f4e0787cdf5
jimmy <foodsksms@gmail.com>
2018-03-22 16:49:34 +0900

    ranks에 들어가는 값들 수정

--- src/main/java/Decision.java
+++ src/main/java/Decision.java
@@ -4,19 +4,38 @@ import java.util.List;
 public class Decision {
     ArrayList<Rank> ranks = new ArrayList<>();
 
+//    public void addRank(List<String> rightNum, ArrayList<Lotto> lottos, String bonusNum) {
+//        for(int i = 0; i < lottos.size(); i++)
+//            decision(lottos.get(i), rightNum, bonusNum);
+//    }
+
+//    public void decision(Lotto lotto, List<String> rightNum, String bonusNum) {
+//        int count = 0;
+//        for(int i = 0; i < rightNum.size(); i++) {
+//            if(lotto.getLotto().contains(Integer.parseInt(rightNum.get(i)))) {
+//                count++;
+//                decisionRank(lotto, count, bonusNum);
+//            }
+//        }
+//    }
+
     public void addRank(List<String> rightNum, ArrayList<Lotto> lottos, String bonusNum) {
-        for(int i = 0; i < lottos.size(); i++)
-            decision(lottos.get(i), rightNum, bonusNum);
+        for(int i = 0; i < lottos.size(); i++) {
+            deliverCount(lottos.get(i), rightNum, bonusNum);
+        }
+
+        for (int i = 0; i < lottos.size(); i++)
+            decisionRank(lottos.get(i), deliverCount(lottos.get(i), rightNum, bonusNum), bonusNum);
     }
 
-    public void decision(Lotto lotto, List<String> rightNum, String bonusNum) {
+    public Integer deliverCount(Lotto lotto, List<String> rightNum, String bonusNum) {
         int count = 0;
         for(int i = 0; i < rightNum.size(); i++) {
             if(lotto.getLotto().contains(Integer.parseInt(rightNum.get(i)))) {
                 count++;
-                decisionRank(lotto, count, bonusNum);
             }
         }
+        return count;
     }
 
     public ArrayList<Rank> decisionRank(Lotto lotto ,int count, String bonusNum) {
@@ -34,7 +53,7 @@ public class Decision {
 
     public void printRanks() {
         for(int i = 0; i < ranks.size(); i++)
-            System.out.println("value of rank is " + ranks.get(i).getCountOfMatch());
+            System.out.println("맞춘갯수 " + ranks.get(i).getCountOfMatch());
         System.out.println("size of ranks is " + ranks.size());
     }
 }
