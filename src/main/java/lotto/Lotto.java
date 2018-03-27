9bc3a1711d51acbbf2007038288f989acfa772a6
jimmy <foodsksms@gmail.com>
2018-03-27 18:08:58 +0900

    수동구매 다시 구현전

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import input.Input;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private List<Integer> lotto;
     private List<Integer> right;
+    private List<Integer> lottoNumber;
+    private List<String> str;
     static ArrayList<Integer> counts = new ArrayList<>();
     static List<Integer> numbers;
     static {
@@ -21,8 +25,16 @@ public class Lotto {
         }
     }
 
+    public Lotto(String string) {
+        str = Arrays.asList(string.split(","));
+        lottoNumber = new ArrayList<>();
+        for (String s: str) {
+            lottoNumber.add(Integer.parseInt(s));
+        }
+
+    }
+
     public Lotto(ArrayList<Integer> input) {
-        right = new ArrayList<>();
         right = input;
     }
 
