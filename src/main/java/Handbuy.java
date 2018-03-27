9a4e5dd398175bd35220a0a0b62b2a15a8fa6665
jimmy <foodsksms@gmail.com>
2018-03-27 10:17:24 +0900

    2018-3-27 Lotto step3 start

---
+++ src/main/java/Handbuy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Handbuy {
+    public static Integer handBuy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+}
