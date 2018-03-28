a072047feb44317835558cec44ea8b66da69319a
JaeyeonKIM <foodsksms@gmail.com>
2018-03-28 20:21:43 +0900

    step3 피드백 수정전

--- src/main/java/lotto/HandLotto.java
+++ src/main/java/lotto/HandLotto.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HandLotto {
-    private lotto.Lotto lotto;
+    private Lotto lotto;
     public ArrayList<lotto.Lotto> generateHandLotto(int handbuy, ArrayList<lotto.Lotto> lottos) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
