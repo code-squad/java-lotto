30845c5a009c3ae88dacbb0800b1e998e9dcb06d
David <xodnjs2549@naver.com>
2018-04-02 15:12:18 +0900

    Test : add some testcase getLottoTicekt, getLottoTickets

--- src/main/java/domain/LottoMain.java
+++ src/main/java/domain/LottoMain.java
@@ -1,6 +1,6 @@
 package domain;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LottoMain {
     public static int getPurchaseFee() {
@@ -10,5 +10,30 @@ public class LottoMain {
 
     public static int getTicketCount(int purchaseFee) {
         return purchaseFee / 1000;
+    }
+
+    public static List getLottoTicket() {
+        Random random = new Random();
+         // 0 ~ 44
+        List<Integer> ticket = new ArrayList<>();
+        while (ticket.size() < 6) {
+            int randomNumber = random.nextInt(45);
+            if (ticket.contains(randomNumber)) {
+                continue;
+            }
+            ticket.add(randomNumber);
+        }
+        ticket.sort(Integer::compareTo);
+        System.out.println(ticket);
+        return ticket;
+    }
+
+    public static List getLottoTickets(int ticketCount) {
+        List<List> lottoTickets = new ArrayList<>();
+        while (lottoTickets.size() < ticketCount) {
+            lottoTickets.add(getLottoTicket());
+        }
+        System.out.println(lottoTickets);
+        return lottoTickets;
     }
 }
--- src/test/java/LottoMainTest.java
+++ src/test/java/LottoMainTest.java
@@ -1,12 +1,16 @@
 import domain.LottoMain;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class LottoMainTest {
     // todo 1. 로또 구매 금액 입력받기
     // todo 2. 로또 금액을 1000(원)으로 나누기
     // todo 3. 로또 몇장 구매했는지 출력하기
+    // todo 4. 로또 번호 생성하기 - 랜덤넘버
+    // todo 5. 생성된 로또 번호들 출력하기
 
 
     @Test
@@ -18,5 +22,15 @@ public class LottoMainTest {
     public void getTicketCount() {
         int purchaseFee = LottoMain.getPurchaseFee();
         assertEquals(10, LottoMain.getTicketCount(purchaseFee));
+    }
+
+    @Test
+    public void getLottoTicket() {
+        assertEquals(6, LottoMain.getLottoTicket().size());
+    }
+
+    @Test
+    public void getLottoTickets() {
+        assertEquals(10, LottoMain.getLottoTickets(LottoMain.getTicketCount(LottoMain.getPurchaseFee())).size());
     }
 }
