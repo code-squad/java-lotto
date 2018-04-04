509729139defec5db8488364a09bacd34bcdc210
larryjung <corbswjd@gmail.com>
2018-04-04 11:33:57 +0900

    lotto process first commit
    1. templates engine handlebar fix
    2. main method refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,21 @@ import domain.LottoNo;
 import domain.LottoNoGroup;
 import domain.User;
 import domain.exceptions.LottoProcessException;
+import spark.ModelAndView;
+import spark.Request;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 import view.InputView;
 import view.ResultView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static domain.LottoUtils.TICKET_PRICE;
+import static domain.LottoUtils.inputParser;
+import static spark.Spark.*;
 
 public class Main {
 
@@ -43,9 +51,38 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        buyProcess();
-        resultProcess();
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 
+    private static Map<String, Object> buyLotto(Request req) {
+        Map<String, Object> input = new HashMap<>();
+        String[] manualNumbers = req.queryParams("manualNumber").split("\r?\n");
+        List<LottoNoGroup> manualInput = new ArrayList<>();
+        for (int i = 0; i < manualNumbers.length; i++) {
+            manualInput.add(inputParser(manualNumbers[i].trim()));
+        }
+        larry = User.whoHasMoneyOf(Integer.parseInt(req.queryParams("inputMoney")));
+        int autoNum = Integer.parseInt(req.queryParams("inputMoney")) / TICKET_PRICE - manualInput.size();
+        larry.purchaseTicketsManual(manualInput);
+        larry.purchaseTicketsAuto(autoNum);
+        input.put("larry", larry);
+        return input;
+    }
+
+    private static Map<String, Object> matchLotto(Request req) {
+        LottoNo bonusNo = LottoNo.of(Integer.parseInt(req.queryParams("bonusNumber")));
+        LotteryCommission.selectWinningNumbers(inputParser(req.queryParams("winningNumber")), bonusNo);
+        larry.checkTotalResult();
+        Map<String, Object> output = new HashMap<>();
+        output.put("larry", larry);
+        return output;
+    }
+    public static void main(String[] args) {
+        port(8080);
+        staticFiles.location("/template");
+        get("/", (req, res) -> render(null, "index.html"));
+        post("/buyLotto", (req, res) -> render(buyLotto(req), "show.html"));
+        post("/matchLotto", (req, res) -> render(matchLotto(req), "result.html"));
+    }
 }
