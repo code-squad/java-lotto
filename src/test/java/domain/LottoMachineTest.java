aa83d654ca1155c1aa7d697b7bdab7e5d9024786
David <xodnjs2549@naver.com>
2018-04-06 14:40:21 +0900

    Refactor : remove not used test

--- src/test/java/domain/LottoMachineTest.java
+++ src/test/java/domain/LottoMachineTest.java
@@ -9,22 +9,10 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class LottoMachineTest {
-//
-//    @Test
-//    public void getTotalCount() {
-//        assertEquals(10, LottoMachine.getTotalCount(10000));
-//    }
-//
-//    @Test
-//    public void getAutoCount() {
-//        assertEquals(8, LottoMachine.getAutoCount(10, 2));
-//    }
-
     @Test
     public void getManualTickets() {
         LottoMachine lottoMachine = new LottoMachine();
-        // 2개의 수동 입력이 들어올
-        // 때, 2개의 LottoTicket을 만들어 내는가?
+        // 2개의 수동 입력이 들어올 때, 2개의 LottoTicket을 만들어 내는가?
         List<String> numbers = new ArrayList<>(Arrays.asList("1,2,3,4,5,6", "7,8,9,10,11,12"));
         List<LottoTicket> lottoTickets = lottoMachine.getManualTickets(numbers);
         assertEquals(2, lottoTickets.size());
