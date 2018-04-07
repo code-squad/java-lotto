a1b13c614f5a9cf7aa2e54910610831e075bf303
SeogMin <csm0594@gmail.com>
2018-04-07 17:34:37 +0900

    TddCaculator

---
+++ src/main/java/lottoGame/Calculator.java
@@ -0,0 +1,74 @@
+package lottoGame;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+    public static int addOperate(String text) {
+
+        return isEmptyValue(text) ? 0 : sumValue(toInts(getSplit(getPureText(text),getCustomDelimetor(text))));
+    }
+
+    private static String getPureText(String text) {
+        Matcher m = getMatcher(text);
+        if(m.find()) {
+            return m.group(2);
+        }
+        return text;
+    }
+
+    private static String getCustomDelimetor(String text) {
+        // java.util.regex 패키지의 Matcher, Pattern import
+        Matcher m = getMatcher(text);
+        String customDelimetor = null;
+        if (m.find()) {
+            customDelimetor = m.group(1);
+        }
+        return customDelimetor;
+    }
+
+    private static Matcher getMatcher(String text) {
+        return Pattern.compile("//(.)\n(.*)").matcher(text);
+    }
+
+    private static boolean isEmptyValue(String s) {
+        if(s == "" || s == null) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] returnValues = new int[values.length];
+        int i = 0;
+        for(String value:values) {
+            returnValues[i++] = isMinusValue(Integer.valueOf(value));
+        }
+        return returnValues;
+    }
+
+    private static int isMinusValue(int value) {
+        if(value < 0) {
+            throw new RuntimeException();
+        }
+        return value;
+    }
+
+    private static int sumValue(int[] values) {
+       int sum = 0;
+       for(int value:values) {
+           sum += value;
+       }
+       return sum;
+    }
+
+    private static Integer getIntegerValue(String operator) {
+        return Integer.valueOf(operator);
+    }
+
+
+    private static String[] getSplit(String operators,String customDelimeter) {
+        return operators.split(",|:"+"|"+customDelimeter);
+    }
+}
