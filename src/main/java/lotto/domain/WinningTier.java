059aaf3dc1c3f8c99bdf4d4a60b5d76a52d408a9
sangsik.kim <sskim@rodaloft.com>
2018-04-10 04:50:20 +0900

    인스턴스 변수명 수정

--- src/main/java/lotto/domain/WinningTier.java
+++ src/main/java/lotto/domain/WinningTier.java
@@ -6,20 +6,20 @@ package lotto.domain;
 public enum WinningTier {
     FAIL(0, 0), FOURTH(3, 5000), THIRD(4, 50000), SECOND(5, 1500000), FIRST(6, 2000000000);
 
-    private Integer value;
-    private Integer prize;
+    private Integer equalCount;
+    private Integer prizeAmount;
 
-    WinningTier(Integer value, Integer prize) {
-        this.value = value;
-        this.prize = prize;
+    WinningTier(Integer equalCount, Integer prizeAmount) {
+        this.equalCount = equalCount;
+        this.prizeAmount = prizeAmount;
     }
 
     public Integer equalCount() {
-        return this.value;
+        return this.equalCount;
     }
 
     public Integer prizeAmount() {
-        return this.prize;
+        return this.prizeAmount;
     }
 
     public static WinningTier get(Integer value) {
