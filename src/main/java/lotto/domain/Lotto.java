47be538ae716fd28593ae0aea9ed1da02313a151
ssosso <ssossohow@gmail.com>
2018-04-11 07:21:03 +0900

    로또 1단계-피드백 반영

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,13 +2,20 @@ package lotto.domain;
 
 public class Lotto {
 	public static final int LOTTO_PRICE = 1000;
+	public static final int LOTTO_NUMBERS_SIZE = 6;
 	private Numbers numbers;
 	
 	public Lotto(Numbers numbers) {
+		if(numbers.size() != LOTTO_NUMBERS_SIZE) {
+			throw new IllegalArgumentException("Lotto는 6개의 번호를 가져야 합니다.");
+		}
 		this.numbers = numbers;
 	}
 	
 	public Lotto(int... numbers) {
+		if(numbers.length != LOTTO_NUMBERS_SIZE) {
+			throw new IllegalArgumentException("Lotto는 6개의 번호를 가져야 합니다.");
+		}
 		this.numbers = new Numbers(numbers);
 	}
 	
@@ -16,7 +23,7 @@ public class Lotto {
 		return numbers;
 	}
 	
-	public int countMatchNumbers(Numbers winNumbers) {
+	public long countMatchNumbers(Numbers winNumbers) {
 		return numbers.countMatch(winNumbers);
 	}
 }
--- src/main/java/lotto/domain/Numbers.java
+++ src/main/java/lotto/domain/Numbers.java
@@ -21,19 +21,12 @@ public class Numbers {
 		this.numbers = numbers.stream().collect(toList());
 	}
 
-	public int countMatch(Numbers numbers2) {
-		int count = 0;
-		Numbers n1 = clone().sort();
-		Numbers n2 = numbers2.clone().sort();
-		for(int i = 0, j = 0, iSize = n1.size(), jSize = n2.size(); i < iSize && j < jSize; i++, j++) {
-			int compareValue = n1.get(i).compareTo(n2.get(j));
-			count += compareValue == 0 ? 1 : 0;
-			i += compareValue < 0 ? 0 : -1;
-			j += compareValue < 0 ? -1 : 0;
-		}
-		return count;
+	public long countMatch(Numbers numbers2) {
+		return numbers2.stream()
+				.filter(number -> numbers.contains(number))
+				.count();
 	}
-	
+
 	public Numbers subNumbers(int start, int end) {
 		return new Numbers(numbers.subList(start, end));
 	}
