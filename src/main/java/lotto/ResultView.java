fdc7f7128a2c75e9eb6fd4584287846ee4d2d63e
jjori.master <jjori.master@gmail.com>
2018-04-11 01:20:01 +0900

    코드 리뷰 수정 사항 수정   - GameResult 등수별 당첨 현황 정보가 List에서 int형으로 변경 됨에 따라 누적 당첨 갯수를 구하는 메소드 변경

--- src/main/java/lotto/ResultView.java
+++ src/main/java/lotto/ResultView.java
@@ -1,12 +1,10 @@
 package lotto;
 
-import java.util.List;
-
 public class ResultView {
-    private static final int THREE_MATCH = 3;
-    private static final int FOUR_MATCH = 4;
-    private static final int FIVE_MATCH = 5;
-    private static final int SIX_MATCH = 6;
+    static final int THREE_MATCH = 3;
+    static final int FOUR_MATCH = 4;
+    static final int FIVE_MATCH = 5;
+    static final int SIX_MATCH = 6;
 
     private static final int THREE_MATCH_MONEY = 5000;
     private static final int FOUR_MATCH_MONEY = 50000;
@@ -14,8 +12,8 @@ public class ResultView {
     private static final int SIX_MATCH_MONEY = 2000000000;
 
     public static void printLotto(LottoGame lottoGame) {
-        for (Lotto lotto : lottoGame.lottos) {
-            System.out.println(lotto.numbers);
+        for (Lotto lotto : lottoGame.getPurchasedLotto()) {
+            System.out.println(lotto.getNumbers());
         }
         System.out.println();
     }
@@ -23,12 +21,16 @@ public class ResultView {
     public static void statistic(GameResult result, int money) {
         System.out.println("당첨 통계");
         System.out.println("-----------------------------------------------");
-        System.out.println("3개일치 (5000원) - " + getWinLottosCount(result, THREE_MATCH));
-        System.out.println("4개일치 (50000원) - " + getWinLottosCount(result, FOUR_MATCH));
-        System.out.println("5개일치 (1500000원) - " + getWinLottosCount(result, FIVE_MATCH));
-        System.out.println("6개일치 (2000000000원) - " + getWinLottosCount(result, SIX_MATCH));
+        System.out.println("3개일치 (5000원) - " + getWinningCount(result, THREE_MATCH));
+        System.out.println("4개일치 (50000원) - " + getWinningCount(result, FOUR_MATCH));
+        System.out.println("5개일치 (1500000원) - " + getWinningCount(result, FIVE_MATCH));
+        System.out.println("6개일치 (2000000000원) - " + getWinningCount(result, SIX_MATCH));
         System.out.println();
         System.out.println("총 수익률은 " + rateOfReturn(getTotal(result), money) + "%입니다.");
+    }
+
+    private static int getWinningCount(GameResult result, int matchNumber) {
+        return result.getWinningCount(matchNumber);
     }
 
     private static int rateOfReturn(int total, int money) {
@@ -37,18 +39,10 @@ public class ResultView {
 
     private static int getTotal(GameResult result) {
         int total = 0;
-        total += getWinLottosCount(result, THREE_MATCH) * THREE_MATCH_MONEY;
-        total += getWinLottosCount(result, FOUR_MATCH) * FOUR_MATCH_MONEY;
-        total += getWinLottosCount(result, FIVE_MATCH) * FIVE_MATCH_MONEY;
-        total += getWinLottosCount(result, SIX_MATCH) * SIX_MATCH_MONEY;
+        total += getWinningCount(result, THREE_MATCH) * THREE_MATCH_MONEY;
+        total += getWinningCount(result, FOUR_MATCH) * FOUR_MATCH_MONEY;
+        total += getWinningCount(result, FIVE_MATCH) * FIVE_MATCH_MONEY;
+        total += getWinningCount(result, SIX_MATCH) * SIX_MATCH_MONEY;
         return total;
-    }
-
-    private static int getWinLottosCount(GameResult result, int threeMatch) {
-        return getLottoListCount(result, threeMatch).size();
-    }
-
-    private static List<Lotto> getLottoListCount(GameResult result, int threeMatch) {
-        return result.winLottos.get(threeMatch);
     }
 }
