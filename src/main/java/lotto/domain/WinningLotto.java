30543d5bf33023a76eb5749851b0bbe1b1eb1deb
KangHyunGu <hkkang@woowahan.com>
2018-04-13 21:53:16 +0900

    로또 2단계 - 피드백 반영

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -10,11 +10,25 @@ public class WinningLotto {
 
     public WinningLotto(List<String> numbers, String bonusNumber) {
         this.lotto = new Lotto(numbers);
+
+        valid(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
     public Rank match(Lotto lotto) {
         return Rank.valueOf(this.lotto.countOfMatch(lotto), this.lotto.contains(bonusNumber));
+    }
+
+    private void valid(String bonusNumber) {
+        Integer number = Integer.parseInt(bonusNumber);
+
+        if (number < 1 || 45 < number) {
+            throw new IllegalArgumentException();
+        }
+
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
