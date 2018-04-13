e49176ddc8e5d747bfa8982c0e3adde67bf69fca
ssosso <ssossohow@gmail.com>
2018-04-13 14:14:49 +0900

    로또 3단계-테스트 코드 커밋에 추가 안되어 추가

--- src/test/java/lotto/domain/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/domain/RandomLottoGeneratorTest.java
@@ -1,4 +1,21 @@
 package lotto.domain;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RandomLottoGeneratorTest {
+	private RandomLottoGenerator randomLottoGenerator;
+	
+	@Before
+	public void setUp() throws Exception {
+		randomLottoGenerator = new RandomLottoGenerator();
+	}
+	
+	@Test
+	public void generate테스트() {
+		assertThat(randomLottoGenerator.generate(0).size())
+				.isEqualTo(0);
+	}
 }
--- src/test/java/lotto/util/StringFormatMatchUtilsTest.java
+++ src/test/java/lotto/util/StringFormatMatchUtilsTest.java
@@ -1,4 +1,22 @@
 package lotto.util;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringFormatMatchUtilsTest {
+	@Test
+	public void matchNumberFormat테스트() {
+		assertThat(StringFormatMatchUtils.matchNumberFormat("kk")).isEqualTo(false);
+		assertThat(StringFormatMatchUtils.matchNumberFormat("0")).isEqualTo(true);
+	}
+	
+	@Test
+	public void matchNumbersFormat테스트() {
+		assertThat(StringFormatMatchUtils.matchNumbersFormat("1, 2,3")).isEqualTo(true);
+		assertThat(StringFormatMatchUtils.matchNumbersFormat("1,2,3")).isEqualTo(true);
+		assertThat(StringFormatMatchUtils.matchNumbersFormat("1, 2,3,")).isEqualTo(false);
+		assertThat(StringFormatMatchUtils.matchNumbersFormat("1,,,, 2,3")).isEqualTo(false);
+		assertThat(StringFormatMatchUtils.matchNumbersFormat("1 2,3")).isEqualTo(false);
+	}
 }
