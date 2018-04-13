e3c1fb8fdddd4d741ab7d6d4afe1f51d1872282c
kjnam <kyujnam@gmail.com>
2018-04-13 22:55:05 +0900

    로또: 3단계

--- src/main/java/lotto/ConsoleMain.java
+++ src/main/java/lotto/ConsoleMain.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.domain.GameResult;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoTicketIssuer;
+import lotto.domain.LottoTickets;
 import lotto.domain.WinningTicket;
 import lotto.view.ConsoleInputView;
 import lotto.view.ConsoleOutputView;
@@ -10,24 +11,38 @@ import lotto.view.ConsoleOutputView;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsoleMain {
     public static void main(String[] args) {
         // 1. 구입금액 입력
         int pay = ConsoleInputView.getPay();
+        int purchasableTicketCount = LottoTicketIssuer.getPurchasableTicketCount(pay);
 
-        // 2. 구입 결과 출력
-        List<LottoTicket> lottoTickets = LottoTicketIssuer.issue(pay);
-        ConsoleInputView.getPayResult(lottoTickets);
+        // 2. 수동 로또 구매
+        int selfSelectLottoCount = ConsoleInputView.getSelfSelectLottoCount();
+        if (selfSelectLottoCount > purchasableTicketCount) {
+            throw new IllegalArgumentException("구매 가능한 로또 수를 초과했습니다.");
+        }
 
-        // 3. 당첨번호 입력
+        ConsoleInputView.printInputSelfNumbersMessage();
+        List<LottoTicket> selfLottoTickets = IntStream.range(0, selfSelectLottoCount)
+                .mapToObj(i -> LottoTicketIssuer.issue(toList(ConsoleInputView.getLottoNumbers())))
+                .collect(Collectors.toList());
+
+        // 3. 구입 결과 출력
+        int restMoney = pay - LottoTicketIssuer.getTicketPrice(selfSelectLottoCount);
+        LottoTickets lottoTickets = new LottoTickets(selfLottoTickets, LottoTicketIssuer.issue(restMoney));
+        ConsoleOutputView.getPayResult(lottoTickets);
+
+        // 4. 당첨번호 입력
         String winningNumber = ConsoleInputView.getWinningNumbers();
         
-        // 4. 보너스번호 입력
+        // 5. 보너스번호 입력
         int bonusNumber = ConsoleInputView.getBonusNumber();
         GameResult gameResult = new GameResult(lottoTickets, new WinningTicket(toList(winningNumber), bonusNumber));
 
-        // 4. 당첨통계 출력
+        // 6. 당첨통계 출력
         ConsoleOutputView.printResult(gameResult.analyzeResult());
         ConsoleOutputView.printRateOfReturn(gameResult.getRateOfReturn());
     }
--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -1,16 +1,14 @@
 package lotto.view;
 
-import lotto.domain.LottoTicket;
-
-import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleInputView {
     private static final String BUY_ORDER_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ORDER_RESULT_MESSAGE_FORMAT = "%s개를 구매했습니다.";
+    private static final String SELF_SELECT_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String SELF_SELECT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
-    
+
     private static final Scanner scanner = new Scanner(System.in, "UTF-8");
 
     private ConsoleInputView() {}
@@ -19,21 +17,31 @@ public class ConsoleInputView {
         System.out.println(BUY_ORDER_MESSAGE);
         int pay = scanner.nextInt();
         scanner.nextLine();
-        
+
         return pay;
     }
 
-    public static void getPayResult(List<LottoTicket> buyTickets) {
-        System.out.println(String.format(ORDER_RESULT_MESSAGE_FORMAT, buyTickets.size()));
-        buyTickets.forEach(System.out::println);
-        System.out.println();
+    public static int getSelfSelectLottoCount() {
+        System.out.println(SELF_SELECT_LOTTO_COUNT_MESSAGE);
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        return count;
+    }
+
+    public static void printInputSelfNumbersMessage() {
+        System.out.println(SELF_SELECT_MESSAGE);
+    }
+
+    public static String getLottoNumbers() {
+        return scanner.nextLine();
     }
 
     public static String getWinningNumbers() {
         System.out.println(WINNING_NUMBERS_MESSAGE);
         return scanner.nextLine();
     }
-    
+
     public static int getBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return scanner.nextInt();
