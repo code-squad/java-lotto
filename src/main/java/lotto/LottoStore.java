f50f95f5e4e1fd0be52d133bf021775eec135df5
jjori.master <jjori.master@gmail.com>
2018-04-18 00:12:47 +0900

    수동구매 로또 가격을 제외한 나머지 금액으로 자동 로또 구매   - ( 구매금액 - 수동구매 로또리스트 사이즈 x 1000원 )   - 자동구매 금액이 모자랄 경우 빈 리스트 반환

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,14 +1,33 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class LottoStore {
     private static final int LOTTO_PRICE = 1000;
 
     static List<Lotto> buyLotto(int money) {
-        checkLowPayment(money);
+        try {
+            checkLowPayment(money);
 
-        return LottoMachine.publishLotto(avaliableLottoCount(money));
+            return LottoMachine.publishLotto(avaliableLottoCount(money));
+
+        } catch(RuntimeException re) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static List<Lotto> buyLotto(int money, List<Lotto> lottos) {
+        money = deductionMoney(money, lottos.size());
+
+        lottos.addAll(LottoStore.buyLotto(money));
+
+        return lottos;
+    }
+
+    private static int deductionMoney(int money, int size) {
+        money -= size * LOTTO_PRICE;
+        return money;
     }
 
     private static int avaliableLottoCount(int money) {
@@ -24,4 +43,5 @@ class LottoStore {
     private static boolean isLowPayment(int money) {
         return money < LOTTO_PRICE;
     }
+
 }
--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -4,24 +4,45 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class LottoStoreTest {
-    @SuppressWarnings("NonAsciiCharacters")
     @Test
     public void buy_lotto_돈_1000원_로또_1장구매() {
         assertThat(LottoStore.buyLotto(1000)).hasSize(1).isInstanceOf(List.class);
     }
 
-    @SuppressWarnings("NonAsciiCharacters")
     @Test
     public void buy_lotto_돈_14000원_로또_14장구매() {
         assertThat(LottoStore.buyLotto(14000)).hasSize(14).isInstanceOf(List.class);
     }
 
-    @SuppressWarnings("NonAsciiCharacters")
     @Test(expected = RuntimeException.class)
     public void 돈_1000원_이하_RuntimeException메소드() {
         LottoStore.buyLotto(100);
+    }
+
+
+    @Test
+    public void 로또가게에서_5000원으로_수동_3장과_자동_2장_구매() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        assertThat(LottoStore.buyLotto(5_000, lottos)).hasSize(5);
+    }
+
+    @Test
+    public void 로또가게에서_3000원으로_수동_3구매() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        assertThat(LottoStore.buyLotto(3_000, lottos)).hasSize(3);
     }
 }
