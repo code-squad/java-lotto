2529cbd33f7c3e0fea2693afeb104bf049e4e204
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-04-23 11:39:38 +0900

    step4 v1

--- src/main/java/LottoExe.java
+++ src/main/java/LottoExe.java
@@ -1,3 +1,4 @@
+/*
 import domain.BuyLotto;
 import domain.Lotto;
 import domain.Rank;
@@ -32,3 +33,4 @@ public class LottoExe {
         Output.resultView(result);
     }
 }
+*/
--- src/main/java/WebLotto.java
+++ src/main/java/WebLotto.java
@@ -1,5 +1,4 @@
-import domain.BuyLotto;
-import domain.Lotto;
+import domain.*;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,6 +8,8 @@ import static spark.Spark.*;
 
 public class WebLotto {
 
+    static List<Lotto> lottos = new ArrayList<>();
+
     public static void main(String[] args) {
         staticFiles.location("/templates");
         port(8080);
@@ -17,12 +18,28 @@ public class WebLotto {
             Map<String, Object> model = new HashMap<>();
             int inputMoney = Integer.parseInt(req.queryParams("inputMoney"));
             List<String> manualNumber = Arrays.asList(req.queryParams("manualNumber").split("\r?\n"));
-            BuyLotto purchaseLotto = new BuyLotto(inputMoney, manualNumber);
-            List<Lotto> lottos = purchaseLotto.getLottos();
 
+            BuyLotto purchaseLotto = new BuyLotto(inputMoney, manualNumber);
+            lottos = purchaseLotto.getLottos();
+
+            int lottoSize = lottos.size();
+
+            model.put("lottoSize", lottoSize);
             model.put("lottos", lottos);
 
             return render(model, "show.html");
+        });
+
+        post("/matchLotto" , (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String winningLotto = req.queryParams("winningNumber");
+            int bonusBall = Integer.parseInt(req.queryParams("bonusNumber"));
+
+            WinningNum winningNum = new WinningNum(winningLotto, bonusBall);
+            Map<Rank, Integer> result = winningNum.checkRank(lottos);
+            Result resultLotto = new Result(result);
+            model.put("resultLotto", resultLotto);
+            return render(model, "result.html");
         });
     }
 
