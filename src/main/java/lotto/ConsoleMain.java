0ce3240e5252b331e59b7e9236a91c13667bb9dd
cardi <graytraces@gmail.com>
2018-08-12 11:43:58 +0900

    코드 리뷰 사항 수정 (part2)
    - while문 대신에 재귀 사용으로 변경

--- src/main/java/lotto/ConsoleMain.java
+++ src/main/java/lotto/ConsoleMain.java
@@ -2,41 +2,37 @@ package lotto;
 
 import java.util.List;
 
+import static lotto.LottoGames.LOTTO_PRICE_PER_TICKET;
+
 public class ConsoleMain {
+
     public static void main(String[] args) {
+        LottoGames lottoGames = getInputAndPlayGame();
+
+        ResultView.printHavingLotto(lottoGames);
+        ResultView.printWinResult(lottoGames.getHavingCount() * LOTTO_PRICE_PER_TICKET , lottoGames.getRankMap());
+
+    }
+
+    public static LottoGames getInputAndPlayGame(){
         LottoGames lottoGames = null;
         long buyAmt  = 0;
         long manualCount = -1;
-        while(true){
-            try{
-                buyAmt =  InputView.getInputPrice();
-                //수동갯수 입력 받기
-                manualCount = InputView.getManualCount();
-                //수동입력 번호 받기
-                String[] manualNumbers = InputView.getManualNumbers(manualCount);
-                lottoGames = new LottoGames(buyAmt, manualNumbers);
 
-            }catch (IllegalArgumentException e){
-                System.out.println("ERROR : " + e.getMessage());
-                continue;
-            }
-            break;
+        try{
+            buyAmt =  InputView.getInputPrice();
+            //수동갯수 입력 받기
+            manualCount = InputView.getManualCount();
+            //수동입력 번호 받기
+            String[] manualNumbers = InputView.getManualNumbers(manualCount);
+            lottoGames = new LottoGames(buyAmt, manualNumbers);
+
+        }catch (IllegalArgumentException e){
+            System.out.println("ERROR : " + e.getMessage());
+            getInputAndPlayGame();
         }
 
-        while(true) {
-            try{
-                List<String> winNumbers = InputView.getWinNumbers();
-                lottoGames.setWinLotto(winNumbers);
-            }catch (IllegalArgumentException e){
-
-                System.out.println("ERROR : " + e.getMessage());
-                continue;
-            }
-            break;
-        }
-
-        ResultView.printHavingLotto(lottoGames);
-        ResultView.printWinResult(buyAmt, lottoGames.getRankMap());
+        return lottoGames;
 
     }
 
